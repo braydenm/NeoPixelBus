283074fb80c22b3c3ed37ee87dea4076c142a339
Brayden McLean <braydenmclean@gmail.com>
2020-12-20 13:32:01 -0800

    remove todo, comment out LRGB

--- src/internal/HD108ColorFeatures.h
+++ src/internal/HD108ColorFeatures.h
@@ -205,7 +205,7 @@ public:
 };
 
 /* RGB Feature -- HD108s ship in RGB order */
-class HD108RgbFeature : public HD1083ElementsNoSettings //TODO modify for HD108
+class HD108RgbFeature : public HD1083ElementsNoSettings
 {
 public:
     static void applyPixelColor(uint8_t* pPixels, uint16_t indexPixel, ColorObject color)
@@ -255,43 +255,43 @@ public:
 
 };
 
-class HD108LrgbFeature : public HD1084ElementsNoSettings //not set up for 16bit yet
-{
-public:
-    static void applyPixelColor(uint8_t* pPixels, uint16_t indexPixel, ColorObject color)
-    {
-        uint8_t* p = getPixelAddress(pPixels, indexPixel);
+// class HD108LrgbFeature : public HD1084ElementsNoSettings //not set up for 16bit yet
+// {
+// public:
+//     static void applyPixelColor(uint8_t* pPixels, uint16_t indexPixel, ColorObject color)
+//     {
+//         uint8_t* p = getPixelAddress(pPixels, indexPixel);
 
-        *p++ = 0xE0 | (color.W < 31 ? color.W : 31); // upper three bits are always 111
-        *p++ = color.R;
-        *p++ = color.G;
-        *p = color.B;
-    }
+//         *p++ = 0xE0 | (color.W < 31 ? color.W : 31); // upper three bits are always 111
+//         *p++ = color.R;
+//         *p++ = color.G;
+//         *p = color.B;
+//     }
 
-    static ColorObject retrievePixelColor(const uint8_t* pPixels, uint16_t indexPixel)
-    {
-        ColorObject color;
-        const uint8_t* p = getPixelAddress(pPixels, indexPixel);
+//     static ColorObject retrievePixelColor(const uint8_t* pPixels, uint16_t indexPixel)
+//     {
+//         ColorObject color;
+//         const uint8_t* p = getPixelAddress(pPixels, indexPixel);
 
-        color.W = (*p++) & 0x1F; // mask out upper three bits
-        color.R = *p++;
-        color.G = *p++;
-        color.B = *p;
+//         color.W = (*p++) & 0x1F; // mask out upper three bits
+//         color.R = *p++;
+//         color.G = *p++;
+//         color.B = *p;
 
-        return color;
-    }
+//         return color;
+//     }
 
-    static ColorObject retrievePixelColor_P(PGM_VOID_P pPixels, uint16_t indexPixel)
-    {
-        ColorObject color;
-        const uint8_t* p = getPixelAddress((const uint8_t*)pPixels, indexPixel);
+//     static ColorObject retrievePixelColor_P(PGM_VOID_P pPixels, uint16_t indexPixel)
+//     {
+//         ColorObject color;
+//         const uint8_t* p = getPixelAddress((const uint8_t*)pPixels, indexPixel);
 
-        color.W = pgm_read_byte(p++) & 0x1F; // mask out upper three bits
-        color.R = pgm_read_byte(p++);
-        color.G = pgm_read_byte(p++);
-        color.B = pgm_read_byte(p);
+//         color.W = pgm_read_byte(p++) & 0x1F; // mask out upper three bits
+//         color.R = pgm_read_byte(p++);
+//         color.G = pgm_read_byte(p++);
+//         color.B = pgm_read_byte(p);
 
-        return color;
-    }
+//         return color;
+//     }
 
-};
+// };
